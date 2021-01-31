1fc03c67ee821368e89f2379e2bc037c3c49fafb
jaejoon.han <jaejoon.han@crevisse.com>
2021-01-31 22:39:56 +0900

    add pepper

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ express()
   .use(express.static(path.join(__dirname, 'pages')))
   .set('views', path.join(__dirname, 'views'))
   .use(favicon(path.join(__dirname, 'views', 'favicon.ico')))
+  .use(express.static(path.join(__dirname, 'static/public')))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
