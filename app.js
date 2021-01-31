606cdb38f411003995efbd368adc2ea41f41adee
jaejoon.han <jaejoon.han@crevisse.com>
2021-01-31 20:11:27 +0900

    add index html

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 80;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World jj');
+    let url = req.url;
+
+    if (url === '/') {
+        url = '/index.html';
+    }
+
+    if (url === 'favicon.ico') {
+        return res.writeHead(404);
+    }
+
+    res.writeHead(200);
+    res.end(fs.readFileSync(__dirname + url));
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running`);
 });
